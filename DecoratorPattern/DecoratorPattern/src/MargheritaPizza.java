eedf0af36fb4e20e37112a67cb8041a94eae6499
Lauren Ingraham <78576502+laureningy@users.noreply.github.com>
2023-05-19 12:32:04 -0400

    Add files via upload

---
+++ DecoratorPattern/DecoratorPattern/src/MargheritaPizza.java
@@ -0,0 +1,11 @@
+public class MargheritaPizza implements Pizza{
+    
+    public String getDescription(){
+        return "Margherita Pizza";
+    }
+    
+    public double getCost(){
+        return 6.99;
+    }
+
+}
